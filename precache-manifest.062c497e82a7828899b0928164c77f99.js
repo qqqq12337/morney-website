4642bbf1cf5acb1467a753cf7c3fe9e4a79a8b4a
yangbq <yangbq@qq.com>
2021-06-21 15:11:03 +0800

    deploy

---
+++ precache-manifest.062c497e82a7828899b0928164c77f99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0a8c7c1df4938e86f9ae251446fdca",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "0082c21d94618f3ac661",
+    "url": "/morney-website/static/css/main.47f2af02.chunk.css"
+  },
+  {
+    "revision": "d57e4168bd7743ff02fa",
+    "url": "/morney-website/static/js/2.df98eed3.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/morney-website/static/js/2.df98eed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0082c21d94618f3ac661",
+    "url": "/morney-website/static/js/main.a3df9e96.chunk.js"
+  },
+  {
+    "revision": "27e129897161fba42a04",
+    "url": "/morney-website/static/js/runtime-main.c2753203.js"
+  }
+]);
